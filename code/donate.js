31144fa1a8ef725eff947a1fd7674714fcc89e08
Joel Serino <joel@thinq4yourself.com>
2022-12-05 13:34:09 -0800

    more style updates and adding givebutter

---
+++ code/donate.js
@@ -0,0 +1,8 @@
+<script>
+    window.Givebutter=window.Givebutter||function(){(Givebutter.q=Givebutter.q||[]).push(arguments)};Givebutter.l=+new Date;
+    window.Givebutter('setOptions', {
+        accountId: "cRjvJC95XQndD0th",
+    });
+</script>
+<script async src="https://js.givebutter.com/elements/latest.js"></script>
+<script src="https://givebutter.com/js/widget.js"></script>
